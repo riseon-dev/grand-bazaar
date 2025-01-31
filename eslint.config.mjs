7d0c4645702540257164f03ef26e833844d76928
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-02-01 02:39:29 +0530

    contract initial

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,17 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  includeIgnoreFile(gitignorePath),
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
