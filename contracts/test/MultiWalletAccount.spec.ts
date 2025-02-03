72bf8f49d3085b3a7973bb15d156392e46ad9523
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-02-03 14:55:41 +0530

    prettier

--- contracts/test/MultiWalletAccount.spec.ts
+++ contracts/test/MultiWalletAccount.spec.ts
@@ -1,9 +1,7 @@
-import {
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
-import { expect } from "chai";
-import hre from "hardhat";
-import {getAddress, parseGwei} from "viem";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers"
+import { expect } from "chai"
+import hre from "hardhat"
+import { getAddress, parseGwei } from "viem"
 //import {
 //   createPublicClient,
 //   createWalletClient,
@@ -18,74 +16,72 @@ import {getAddress, parseGwei} from "viem";
 //   parseGwei
 // } from 'viem';
 
+async function deployContracts() {
+	const [deployer, operator, user1] = await hre.viem.getWalletClients()
 
-async function deployContracts () {
-  const [deployer, operator, user1] = await hre.viem.getWalletClients();
+	const publicClient = await hre.viem.getPublicClient()
 
-  const publicClient = await hre.viem.getPublicClient();
+	const deployStableCoin = async () => {
+		const stableCoin = await hre.viem.deployContract(
+			"StableCoin",
+			[parseGwei("1000000")],
+			{}
+		)
 
-  const deployStableCoin = async () => {
-    const stableCoin = await hre.viem.deployContract("StableCoin", [
-      parseGwei('1000000'),
-    ], {});
+		return stableCoin
+	}
 
-    return stableCoin;
-  }
+	const deployMultiWallet = async (stableCoinAddress: string) => {
+		// eslint-disable-next-line
+		// @ts-ignore
+		const multiWallet = await hre.viem.deployContract(
+			"MultiWalletAccount",
+			[operator.account.address, stableCoinAddress],
+			{}
+		)
+		return multiWallet
+	}
 
-  const deployMultiWallet = async (stableCoinAddress: string) => {
-    // eslint-disable-next-line
-    // @ts-ignore
-    const multiWallet = await hre.viem.deployContract("MultiWalletAccount", [
-      operator.account.address,
-      stableCoinAddress,
-    ], {});
-    return multiWallet;
-  }
+	const stableCoin = await deployStableCoin()
+	const multiWallet = await deployMultiWallet(stableCoin.address)
 
-  const stableCoin = await deployStableCoin();
-  const multiWallet = await deployMultiWallet(stableCoin.address);
+	return {
+		deployer,
+		operator,
+		user1,
+		stableCoin,
+		multiWallet,
+		publicClient,
+	}
+}
 
-  return {
-    deployer,
-    operator,
-    user1,
-    stableCoin,
-    multiWallet,
-    publicClient,
-  }}
+describe("MultiWalletAccount", function () {
+	describe("Deployment", () => {
+		it("should require one operator and a base token to deploy", async () => {
+			const { stableCoin, multiWallet } = await loadFixture(deployContracts)
 
+			// eslint-disable-next-line
+			expect(stableCoin).to.be.ok
+			// eslint-disable-next-line
+			expect(multiWallet).to.be.ok
+		})
 
+		it("should have the correct operator && stable coin address", async () => {
+			const { operator, multiWallet, stableCoin } = await loadFixture(deployContracts)
 
-describe('MultiWalletAccount', function () {
-  describe('Deployment', () => {
-    it('should require one operator and a base token to deploy', async () => {
-      const { stableCoin, multiWallet } = await loadFixture(deployContracts);
+			// eslint-disable-next-line
+			// @ts-ignore
+			expect(await multiWallet.read.operator()).to.equal(
+				getAddress(operator.account.address)
+			)
 
-      // eslint-disable-next-line
-      expect(stableCoin).to.be.ok;
-      // eslint-disable-next-line
-      expect(multiWallet).to.be.ok;
-    });
+			// eslint-disable-next-line
+			// @ts-ignore
+			expect(await multiWallet.read.baseToken()).to.equal(getAddress(stableCoin.address))
+		})
+	})
 
-    it('should have the correct operator && stable coin address', async () => {
-      const { operator, multiWallet, stableCoin } = await loadFixture(deployContracts);
-
-      // eslint-disable-next-line
-      // @ts-ignore
-      expect(await multiWallet.read.operator()).to.equal(
-        getAddress(operator.account.address)
-      );
-
-      // eslint-disable-next-line
-      // @ts-ignore
-      expect(await multiWallet.read.baseToken()).to.equal(getAddress(stableCoin.address));
-    });
-  });
-
-
-  // describe('Only Operator functions', function () => {
-  //
-  // })
-
-
+	// describe('Only Operator functions', function () => {
+	//
+	// })
 })
---
+++ libraries/prettier-config-custom/index.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line
+module.exports = {
+  "singleQuote": true,
+  "trailingComma": "all",
+  "bracketSpacing": false,
+  "arrowParens": "avoid"
+};
