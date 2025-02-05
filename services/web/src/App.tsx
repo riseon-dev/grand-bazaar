6559af04ef030347beb51e68d1f33b6fc83c711d
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-02-05 21:44:16 +0530

    frontend barebones

---
+++ services/web/src/App.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line
+// @ts-ignore
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
