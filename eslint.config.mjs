8e5c7cfe0523c1ae4f8bb284f3f5a2073aecd399
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-02-05 22:02:33 +0530

    fixed package issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -44,7 +44,7 @@ export default [
       // suppress errors for missing 'import React' in files
       "react/react-in-jsx-scope": "off",
       // allow jsx syntax in js files (for next.js project)
-      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
+      // "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
     },
   },
 ];
