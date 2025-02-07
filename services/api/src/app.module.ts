2ce95ba84d15f04f1136a03f8641602df6991735
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-02-07 12:29:34 +0530

    basic structure

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { WorkflowsModule } from './application/workflows.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [],
-  providers: [],
+  imports: [ConfigModule.forRoot(), WorkflowsModule],
 })
 export class AppModule {}
