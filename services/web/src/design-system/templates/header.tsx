ccc652baf07074e753bd8c454d4db5a1c5682fed
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-02-08 15:56:24 +0530

    header

--- services/web/src/design-system/templates/header.tsx
+++ services/web/src/design-system/templates/header.tsx
@@ -1,13 +1,24 @@
 // import React from 'react';
 import {Link} from '@tanstack/react-router';
+import {Box, Flex} from '@radix-ui/themes';
 
 const Header = () => {
   return (
     <>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
+      <Flex gap="3" justify={{sm: 'end', lg: 'between'}} align="end">
+        <Box>
+          <Link to="/">Home</Link>
+        </Box>
+        <Box>
+          <Link to="/about">About</Link>
+        </Box>
+        <Box>
+          <a href="#">Deploy your own AI bot</a>
+        </Box>
+        <Box>
+          <a href="#">Connect Wallet</a>
+        </Box>
+      </Flex>
     </>
   );
 };
