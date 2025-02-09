eefdb8903f3ee15e5e61425e43edccded597e443
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-02-09 12:47:44 +0530

    adding deploy route

---
+++ services/web/src/design-system/pages/deploy.tsx
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+const Deploy = () => {
+  return <div>Deploy your own bot</div>;
+};
+
+export default Deploy;
--- services/web/src/routes/deploy.tsx
+++ services/web/src/routes/deploy.tsx
@@ -1,8 +1,9 @@
 import {createRoute} from '@tanstack/react-router';
 import {rootRoute} from './__root.tsx';
+import Deploy from '../design-system/pages/deploy.tsx';
 
 export const deployRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: '/deploy',
-  component: () => <div>Deploy</div>,
+  component: () => <Deploy />,
 });
